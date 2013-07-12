d673f8b2b38fcdb0cb18a607b35b83f72fe40085
Gonzalo Abella <abellagonzalo@gmail.com>
2013-07-12 12:02:38 +0200

    Parameter class.

--- parameter_example/include/parameter_server/parameter.h
+++ parameter_example/include/parameter_server/parameter.h
@@ -2,16 +2,21 @@
 #define PARAMETER_H
 
 #include <boost/shared_ptr.hpp>
+#include <set>
 
 namespace parameter_server {
 
-template <class T>
+template <class T, class Subscriber>
 class Parameter
 {
+  typedef std::set<Subscriber> Subscribers;
+
 public:
+  typedef typename Subscribers::iterator iterator;
+
   Parameter()
   : impl_(new Impl)
-  { }
+  { impl_->setted = false; }
 
   ~Parameter()
   { }
@@ -23,15 +28,48 @@ public:
   { impl_ = rhs.impl_; }
 
   bool operator==(const Parameter& rhs) const
-  {
-    return impl_ == rhs.impl_;
+  { return impl_ == rhs.impl_; }
+
+  void setValue(const T& value)
+  { 
+    impl_->value = value;
+    impl_->setted = true;
   }
+
+  void getValue(T& value)
+  { value = impl_->value; }
+
+  bool hasValue()
+  { return impl_->setted; }
+
+  void deleteValue()
+  { impl_->setted = false; }
+
+  void addSubscriber(const Subscriber& subscriber)
+  { impl_->subscribers.insert(subscriber); }
+
+  void removeSubscriber(const Subscriber& subscriber)
+  { impl_->subscribers.erase(subscriber); }
+
+  iterator subscribersBegin()
+  { return impl_->subscribers.begin(); }
+
+  iterator subscribersEnd()
+  { return impl_->subscribers.end(); }
+
+  int subscribersSize()
+  { return impl_->subscribers.size(); }
+
+  bool subscribersEmpty()
+  { return impl_->subscribers.empty(); }
 
 private:
 
   struct Impl
   {
     T value;
+    bool setted;
+    Subscribers subscribers;
   };
   typedef boost::shared_ptr<Impl> ImplPtr;
 
--- parameter_example/test/parameter_test.cpp
+++ parameter_example/test/parameter_test.cpp
@@ -5,18 +5,79 @@ class ParameterServerTest : public testing::Test
 {
 protected:
   typedef std::vector<int> Ints;
+  typedef int Subscriber;
+  typedef parameter_server::Parameter<Ints, Subscriber> MyParameter;
 
-  parameter_server::Parameter<Ints> p;
+  virtual void SetUp()
+  { }
+
+  Ints intsOfSize(int size)
+  {
+    Ints ints(size);
+    for (int i=0; i<size; ++i)
+      ints.push_back(i*10);
+    return ints;
+  }
+
+  MyParameter p;
 };
 
 TEST_F(ParameterServerTest, CopyConstructor) {
-  parameter_server::Parameter<Ints> q(p);
+  MyParameter q(p);
   EXPECT_EQ(p, q);
 }
 
 TEST_F(ParameterServerTest, OperatorEqual) {
-  parameter_server::Parameter<Ints> q = p;
+  MyParameter q = p;
   EXPECT_EQ(p, q);
+}
+
+TEST_F(ParameterServerTest, CanGet) {
+  p.setValue(intsOfSize(10));
+  Ints ints2;
+  p.getValue(ints2);
+  EXPECT_EQ(intsOfSize(10), ints2);
+}
+
+TEST_F(ParameterServerTest, HasValueFalse) {
+  EXPECT_FALSE(p.hasValue());
+}
+
+TEST_F(ParameterServerTest, HasValueTrue) {
+  p.setValue(intsOfSize(10));
+  EXPECT_TRUE(p.hasValue());
+}
+
+TEST_F(ParameterServerTest, DeleteValueNotSetted) {
+  p.deleteValue();
+  EXPECT_FALSE(p.hasValue());
+}
+
+TEST_F(ParameterServerTest, DeleteValueSetted) {
+  p.setValue(intsOfSize(10));
+  p.deleteValue();
+  EXPECT_FALSE(p.hasValue());
+}
+
+TEST_F(ParameterServerTest, CheckSubscribersEmpty) {
+  EXPECT_TRUE(p.subscribersEmpty());
+  EXPECT_EQ(0, p.subscribersSize());
+}
+
+TEST_F(ParameterServerTest, CheckSubscribersNotEmpty) {
+  p.addSubscriber(0);
+  EXPECT_FALSE(p.subscribersEmpty());
+  EXPECT_EQ(1, p.subscribersSize());
+}
+
+TEST_F(ParameterServerTest, IterateThroughSusbcribers) {
+  for (int i=0; i<10; ++i)
+    p.addSubscriber(i);
+  MyParameter::iterator it = p.subscribersBegin();
+  MyParameter::iterator end = p.subscribersEnd();
+  int i = 0;
+  for (; it != end; ++it)
+    EXPECT_EQ(i++, *it);
 }
 
 int main(int argc, char **argv)
